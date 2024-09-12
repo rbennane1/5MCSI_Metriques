32454ec3eb451444537a5f77557d7f508c0d7bca
rbennane1 <ben.rab2000@gmail.com>
2024-09-12 15:45:35 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -31,7 +31,7 @@ def histogramme():
     return render_template("histogramme.html")
 
 @app.route("/contact/")
-def contact ():
+def contact():
     return render_template("contact.html")
                                                                                                                                        
 @app.route('/')
